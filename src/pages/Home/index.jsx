936956cd010ec571b298e26d766251bc939fdf6e
Sergio Medeiros <sergiof11@outlook.com.br>
2022-09-06 13:30:34 -0300

    Organizing files and code

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import './style.css';
+
+function Home() {
+  return (
+    <div className="container">  
+    <h1>Lista de presença</h1>
+    <input type="text" placeholder="Digite o nome aqui ..."/>
+    <button type="button">Adicionar</button>
+    </div>
+  )
+}
+
+export default Home;
