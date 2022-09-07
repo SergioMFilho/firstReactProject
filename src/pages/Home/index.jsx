7cf4859aaf37cc8d44f9253de90b79ca800cc8d0
Sergio Medeiros <sergiof11@outlook.com.br>
2022-09-07 11:26:18 -0300

    Adding functionality to add people to presence list

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,40 @@
 import './style.css';
+import React, {useState} from 'react';  // State is important when we want to store values and be used in the interface
+import {Card} from '../../components/Card';
 
-function Home() {
+
+
+function Home() {                             // Initial value Sérgio that changes when writing characters
+  const [studentName, setStudentName] = useState(); // The first is the variable that stores, the second is the function that updates this state
+  const [students, setStudents] = useState([]);
+
+  function handleAddStudent() {
+    const newStudent = {
+      name : studentName,
+      time : new Date().toLocaleDateString("pt-br", {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+      })
+    };
+
+    setStudents(prevState => [...prevState, newStudent]); // Creating a new student and keeping the one already created
+  }
+
   return (
     <div className="container">  
-    <h1>Lista de presença</h1>
-    <input type="text" placeholder="Digite o nome aqui ..."/>
-    <button type="button">Adicionar</button>
+    <h1>Attendance List</h1>
+    <input 
+    type="text" 
+    placeholder="Enter name here..."
+    onChange={e => setStudentName(e.target.value)}  // Get the value and write it to the console
+    />
+
+    <button type="button" onClick={handleAddStudent}>Add</button>
+    {                           // When we use the keys inside the return, we want to use the contents of a variable (object)
+    students.map(student => <Card name= {student.name} time= {student.time}/> )            
+    
+    }
     </div>
   )
 }
