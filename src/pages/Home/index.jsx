d5345e056dd60dfdaadf4bb3614132d4fe046646
Sergio Medeiros <sergiof11@outlook.com.br>
2022-10-19 14:06:50 -0300

    Adding props and useState

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,19 +1,45 @@
-import './styles.css';
+import React, { useState } from "react";
+import "./styles.css";
 
-import Card from '../../Componentes/Card';
+import Card from "../../Componentes/Card";
 
 function Home() {
+  const [studentName, setStudentName] = useState("");
+  // O primeiro é onde vai armazenar e o segundo e o segundo é a função que armazena esse estado
+  const [students, setStudents] = useState([]);
+
+  function handleAddStudent() {
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleTimeString("pt-br", {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+      }),
+    };
+
+    setStudents((prevState) => [...prevState, newStudent]);
+  }
+
   return (
-    <div className='container'>
-    <h1>Lista de Presença</h1>
-    <input type="text" placeholder="Digite o nome..." />
-    <button type="button">Adicionar</button>
-    
-    <Card name="Rodrigo" time="10:55:25"/>
-    <Card name="Joao" time="12:05:22"/>
-    <Card />
+    <div className="container">
+      <h1>Lista de Presença</h1>
+
+      <input
+        type="text"
+        placeholder="Digite o nome..."
+        onChange={(e) => setStudentName(e.target.value)}
+      />
+
+      <button type="button" onClick={handleAddStudent}>
+        Adicionar
+      </button>
+
+      {students.map((student) => (
+        <Card key={student.time} name={student.name} time={student.time} />
+      ))}
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
