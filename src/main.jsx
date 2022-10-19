005d14cd3849e45e77bb5d5545a296556df4fbd4
Sergio Medeiros <sergiof11@outlook.com.br>
2022-10-19 12:17:12 -0300

    Adding files

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Home from './pages/Home';
-import './styles/global.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './styles/global.css'
+import Home from './pages/Home'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,42 +1,19 @@
-import './style.css';
-import React, {useState} from 'react';  // State is important when we want to store values and be used in the interface
-import {Card} from '../../components/Card';
+import './styles.css';
 
+import Card from '../../Componentes/Card';
 
-
-function Home() {                             // Initial value Sérgio that changes when writing characters
-  const [studentName, setStudentName] = useState(); // The first is the variable that stores, the second is the function that updates this state
-  const [students, setStudents] = useState([]);
-
-  function handleAddStudent() {
-    const newStudent = {
-      name : studentName,
-      time : new Date().toLocaleDateString("pt-br", {
-        hour: "2-digit",
-        minute: "2-digit",
-        second: "2-digit",
-      })
-    };
-
-    setStudents(prevState => [...prevState, newStudent]); // Creating a new student and keeping the one already created
-  }
-
+function Home() {
   return (
-    <div className="container">  
-    <h1>Attendance List</h1>
-    <input 
-    type="text" 
-    placeholder="Enter name here..."
-    onChange={e => setStudentName(e.target.value)}  // Get the value and write it to the console
-    />
-
-    <button type="button" onClick={handleAddStudent}>Add</button>
-    {                           // When we use the keys inside the return, we want to use the contents of a variable (object)
-    students.map(student => <Card name= {student.name} time= {student.time}/> )            
+    <div className='container'>
+    <h1>Lista de Presença</h1>
+    <input type="text" placeholder="Digite o nome..." />
+    <button type="button">Adicionar</button>
     
-    }
+    <Card name="Rodrigo" time="10:55:25"/>
+    <Card name="Joao" time="12:05:22"/>
+    <Card />
     </div>
   )
 }
 
-export default Home;
+export default Home
